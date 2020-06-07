e8acc603f8654174ee494a0e525eddadfc3f85e6
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-06-06 22:27:56 -0600

    user input is stored and rendered to html. styling and final touches will be added next

--- app.js
+++ app.js
@@ -1,106 +1,149 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+const prompt = require("inquirer").prompt;
 const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
-const { prompt } = inquirer;
 const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 const managerPrompt = async () => {
-  const { name, id, email, officeNumber } = await prompt([
-    {
-      name: "name",
-      message: "Manager's Name:",
-    },
-    {
-      name: "id",
-      message: "Manager's Id:",
-    },
-    {
-      name: "email",
-      message: "Manager's Email Address:",
-    },
-    {
-      name: "officeNumber",
-      message: "Manager's Office Phone Number:",
-    },
-  ]);
-  employees.push( new Manager( name, id, email, officeNumber));
+  try {
+    const { name, id, email, officeNumber } = await prompt([
+      {
+        name: "name",
+        message: "Manager's Name:",
+      },
+      {
+        name: "id",
+        message: "Manager's Id:",
+      },
+      {
+        name: "email",
+        message: "Manager's Email Address:",
+      },
+      {
+        name: "officeNumber",
+        message: "Manager's Office Phone Number:",
+      },
+    ]);
+    employees.push(new Manager(name, id, email, officeNumber));
+  } catch (err) {
+    throw err;
+  }
 };
 const internPrompt = async () => {
-    const { name, id, email, school} = prompt([{
+  try {
+    const { name, id, email, school } = await prompt([
+      {
         name: "name",
-        message: "Intern's Name:"
-    },
-    {
+        message: "Intern's Name:",
+      },
+      {
         name: "id",
-        message: "Intern's Id:"
-    },
-    {
+        message: "Intern's Id:",
+      },
+      {
         name: "email",
-        message: "Intern's Email Address:"
-    },
-    {
+        message: "Intern's Email Address:",
+      },
+      {
         name: "school",
-        message: "Intern's School:"
-    }
-]) 
-    employees.push(new Intern( name, id, email, school))
+        message: "Intern's School:",
+      },
+    ]);
+    employees.push(new Intern(name, id, email, school));
+  } catch (err) {
+    throw err;
+  }
 };
+
 const engineerPrompt = async () => {
-  const { name, id, email, github } = await prompt([
-    {
-      name: "name",
-      message: "Engineer's Name:",
-    },
-    {
-      name: "id",
-      message: "Engineer's Id:",
-    },
-    {
-      name: "email",
-      message: "Engineer's Email Address:",
-    },
-    {
-      name: "github",
-      message: "Enter The Engineer's Github:",
-    },
-  ]);
-  employees.push(new Engineer(name, id, email, github));
+  try {
+    const { name, id, email, github } = await prompt([
+      {
+        name: "name",
+        message: "Engineer's Name:",
+      },
+      {
+        name: "id",
+        message: "Engineer's Id:",
+      },
+      {
+        name: "email",
+        message: "Engineer's Email Address:",
+      },
+      {
+        name: "github",
+        message: "Enter The Engineer's Github:",
+      },
+    ]);
+    employees.push(new Engineer(name, id, email, github));
+  } catch (err) {
+    throw err;
+  }
 };
 
+let html;
 const mainPrompt = async () => {
-    await managerPrompt();
-    const { next } = await prompt([{
+  try {
+    const { next } = await prompt([
+      {
         name: "next",
-        message: "Who would you like to add next?:",
+        message: "Who would you like to add?:",
         type: "list",
-        choices: ["Intern", "Engineer", "Manager", "I'm finished"]
-    }])
-    switch (next){
-    case "I'm finished":
-        console.log(employees);
-        render();
-    case "Manager":
-        managerPrompt();
-    case "Intern":
-        internPrompt();
-    case "Engineer":
-        engineerPrompt();
-    }
+        choices: ["Intern", "Engineer", "Manager", "I'm finished"],
+      },
+    ]);
 
-    
-    
+    switch (next) {
+      case "I'm finished":
+        console.log(employees);
+        html = render(employees);
+        return;
+      case "Manager":
+        await managerPrompt();
+        await mainPrompt();
+        return;
+      case "Intern":
+        await internPrompt();
+        await mainPrompt();
+        return;
+      case "Engineer":
+        await engineerPrompt();
+        await mainPrompt();
+        return;
+    }
+  } catch (err) {
+    throw err;
+  }
 };
+
+const write = async () => {
+  try {
+    await fs.mkdirSync(OUTPUT_DIR, (err) => {
+      throw err;
+    });
+    await fs.writeFileSync(outputPath, html, (err) => {
+      throw err;
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const all = async (html) => {
+  try {
+    await mainPrompt();
+    await write(html);
+  } catch (err) {
+    throw err;
+  }
+};
+all();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -120,4 +163,3 @@ const mainPrompt = async () => {
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
-mainPrompt();
